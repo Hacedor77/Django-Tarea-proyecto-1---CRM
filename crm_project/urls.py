30ce69b87ee90eb045056f8f097c14ba66975b50
Hacedor77 <hacedorinmobiliario@gmail.com>
2025-08-02 12:37:13 +0200

    Agregar app CRM - base funcionando correctamente

--- crm_project/urls.py
+++ crm_project/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def home_view(request):
-    return HttpResponse("<h1>¡Django funciona en Render!</h1><p>Setup básico exitoso.</p>")
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'),
+    path('', include('crm.urls')),  # Carga las rutas de la app CRM
 ]
+
